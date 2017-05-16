1a2de4b9d5b96672de526a1312d57ac1edbf162d
Kiricon <kiricon@live.com>
2017-05-16 11:48:01 -0600

    Made code examples fade in on load

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", (event) => { 
+document.addEventListener('DOMContentLoaded', (event) => { 
     let blocks = document.querySelectorAll('pre code');
     for(let i = 0; i < blocks.length; i++) {
-    hljs.highlightBlock(blocks[i]);
+        hljs.highlightBlock(blocks[i]);
+        blocks[i].className += ' show';
     }
 });
