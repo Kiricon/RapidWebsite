2e746a0c68ea8f4afcfae6dc6e10eff3c79e06d5
Kiricon <kiricon@live.com>
2017-05-17 09:02:03 -0600

    Broke out header and footer.

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func main() {
 		c.View("views/index.html")
 	})
 
+	app.Get("/quickstart", func(c rapid.Connection) {
+		c.View("views/quickstart.html")
+	})
+
 	app.StaticFolder("/", "public")
 
 	app.Listen(8080)
